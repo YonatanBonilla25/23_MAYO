60d7e2b2cc714d0579450404c035b6c7dc58bde8
YonatanBonilla25 <137957450+YonatanBonilla25@users.noreply.github.com>
2024-05-23 09:40:16 -0500

    Add files via upload

---
+++ algoritmo hemoglobina/hemoglobina.js
@@ -0,0 +1,9 @@
+
+const genero = document. querySelector ("genero");
+const dias = document. querySelector ("dias");
+const vecor = document. querySelector ("vector");
+
+function hola() {
+    alert("hola vamos a medir tu hemoglobina");
+  }
+  
